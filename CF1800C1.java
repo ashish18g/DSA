e88b66c71722463ddd47408d7d5253c2fc81444b
MrP3rfect <gorleashish@iitbhilai.ac.in>
2026-06-02 11:22:29 +0530

    CF1800C1

---
+++ CF1800C1.java
@@ -0,0 +1,42 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class CF1800C1 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            long[] armies = new long[n];
+            for (int i = 0; i < n; i++) {
+                Deque<Long> bonus_deck = new ArrayDeque<>();
+                int k = Integer.parseInt(sc.next());
+                long army_strength = 0;
+                for (int a = 0; a < k; a++) {
+                    long power = Long.parseLong(sc.next());
+                    if (power == 0) {
+                        if (bonus_deck.isEmpty()) {
+                            continue;
+                        }
+                        army_strength += bonus_deck.pop();
+                    } else if (bonus_deck.isEmpty() || bonus_deck.peek() <= power) {
+                        bonus_deck.push(power);
+                    } else if (bonus_deck.peek() > power) {
+                        Deque<Long> temp_stack = new ArrayDeque<>();
+                        while (!bonus_deck.isEmpty() && bonus_deck.peek() > power) {
+                            temp_stack.push(bonus_deck.pop());
+                        }
+                        bonus_deck.push(power);
+                        while (!temp_stack.isEmpty()) {
+                            bonus_deck.push(temp_stack.pop());
+                        }
+                    }
+                }
+
+                armies[i] = army_strength;
+            }
+            for (long a : armies) {
+                System.out.println(a);
+            }
+        }
+    }
+}
