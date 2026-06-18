8372619d7e574ea0e6acbd01d955b33437ee0b5b
MrP3rfect <gorleashish@iitbhilai.ac.in>
2026-06-18 16:43:06 +0530

    start project

---
+++ src/main/java/ashish/btech/dsa/cf/CF4A.java
@@ -0,0 +1,17 @@
+package ashish.btech.dsa.cf;
+
+import java.util.Scanner;
+
+public class CF4A {
+    public static void main(String[] args) {
+        int w;
+        try (Scanner sc = new Scanner(System.in)) {
+            w = sc.nextInt();
+        }
+        if (w > 3 && w % 2 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
